2edbd2863d3b977ee0bcc58dd798cf3ab17f1bf0
mohammad <mrmy98@gmail.com>
2023-01-16 13:57:26 +0330

    index and Cantact pages added

--- DevCard2/DevCard2/Controllers/HomeController.cs
+++ DevCard2/DevCard2/Controllers/HomeController.cs
@@ -11,23 +11,23 @@ namespace DevCard2.Controllers
 {
 	public class HomeController : Controller
 	{
-		private readonly ILogger<HomeController> _logger;
+	
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController()
 		{
-			_logger = logger;
+			
 		}
 
 		public IActionResult Index()
 		{
 			return View();
 		}
-
-		public IActionResult Privacy()
+		public IActionResult Contact()
 		{
+
 			return View();
 		}
-
+	
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
