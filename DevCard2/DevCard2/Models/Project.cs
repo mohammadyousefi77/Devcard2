1c754a1d8ece06ad95488ded2dd6037fa1ca6f1b
mohammad <mrmy98@gmail.com>
2023-01-16 15:59:59 +0330

    syncronization

---
+++ DevCard2/DevCard2/Models/Project.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace DevCard2.Models
+{
+	public class Project
+	{
+
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string  Client { get; set; }
+
+	}
+	public class Article {
+		public int Id { get; set; }
+		public string Title{ get; set; }
+		public string Description { get; set; }
+
+
+	}
+	public class User {
+		public List<Project> Projects { get; set; }
+		public List<Article> Articles { get; set; }
+
+	}
+
+
+	 
+
+}
